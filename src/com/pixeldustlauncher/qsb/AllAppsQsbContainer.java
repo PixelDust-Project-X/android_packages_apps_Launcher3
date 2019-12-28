821f675fb34e13428434f2757ad508f1d7fe2f6f
Chris Crump <chris@cypheros.co>
2019-12-28 05:26:54 -0500

    PixeldustLauncher: Decrease QSB search task delay
    Change-Id: I2fc47e7cbeae257da1cdd6492c9b0da8d9686f3e
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldustlauncher/qsb/AllAppsQsbContainer.java
+++ src/com/pixeldustlauncher/qsb/AllAppsQsbContainer.java
@@ -83,7 +83,7 @@ import com.pixeldust.launcher.search.SearchHandler;
 
 public class AllAppsQsbContainer extends FrameLayout implements Insettable, OnClickListener, OnChangeListener, OnUpdateListener, SearchUiManager {
 
-    private static final long SEARCH_TASK_DELAY_MS = 700;
+    private static final long SEARCH_TASK_DELAY_MS = 450;
 
     private AllAppsContainerView mAppsView;
     private Bitmap mQsbScroll;
